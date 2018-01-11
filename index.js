81c963210cb393da65cd57d329a97534f13c30e2
Michal Matyjek <michal@gladly.com>
2018-01-10 22:18:26 -0800

    Make lint clean

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+/* eslint-disable no-console */
 const express = require('express');
 
 const app = express();
 app.get('/', (req, res) => res.send('Hello World!'));
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
+/* eslint-enable no-console */
